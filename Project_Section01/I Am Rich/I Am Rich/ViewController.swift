2239ed8c469396ad3a98f780573e0ee63026178d
KasRoid <KasRoid@gmail.com>
2020-05-02 22:12:47 +0900

    Update

---
+++ Project_Section01/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Kas Song on 2020.05.02.
+//  Copyright © 2020 Kas Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
