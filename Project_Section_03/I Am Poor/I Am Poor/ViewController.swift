3603bc19add75eda680c8b331d7c6183b5307d51
KasRoid <KasRoid@gmail.com>
2020-05-03 22:31:56 +0900

    Scetion3 Finished

---
+++ Project_Section_03/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Kas Song on 2020.05.03.
+//  Copyright © 2020 Kas Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
