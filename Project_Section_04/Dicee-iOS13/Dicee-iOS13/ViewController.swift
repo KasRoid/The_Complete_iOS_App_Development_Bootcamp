339c412becfe38e6e7568c58dca5e716f57ac811
KasRoid <KasRoid@gmail.com>
2020-05-04 00:07:39 +0900

    Section 4.39 DONE

--- Project_Section_04/Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Project_Section_04/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
     
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
     
 }
 
