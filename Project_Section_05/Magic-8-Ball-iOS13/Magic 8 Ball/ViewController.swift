269fe862b3e6fa7ba415dd337081171bbd84b658
KasRoid <KasRoid@gmail.com>
2020-05-04 18:46:12 +0900

    Section 5.58 DONE

--- Project_Section_05/Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
+++ Project_Section_05/Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
@@ -11,9 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var askButtonPressed: UIButton!
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
+    let ballArray: Array<UIImage> = [UIImage(named: "ball1")!, UIImage(named: "ball2")!, UIImage(named: "ball3")!, UIImage(named: "ball4")!, UIImage(named: "ball5")!]
+    
+    @IBAction func askButtonPressed() {
+        imageView.image = ballArray.randomElement()
+    }
+    
 }
-
