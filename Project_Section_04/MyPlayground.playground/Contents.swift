094f6a4062a699e39e0a1f152bc7ebc92ad9c331
KasRoid <KasRoid@gmail.com>
2020-05-04 10:51:32 +0900

    Section 4.48 DONE

--- Project_Section_04/MyPlayground.playground/Contents.swift
+++ Project_Section_04/MyPlayground.playground/Contents.swift
@@ -5,3 +5,18 @@ var numbers = [45, 74, 195, 53]
 var computedNumbers = [numbers[0] * numbers[1], numbers[1] * numbers[2], numbers[2] * numbers[3]]
 
 print(computedNumbers)
+
+
+let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+//The number of letters in alphabet equals 26
+
+var password: String = ""
+
+for _ in 1...6 {
+    let rdmAlpahbet = Int.random(in: 0...25)
+    password += alphabet[rdmAlpahbet]
+}
+
+
+print(password)
