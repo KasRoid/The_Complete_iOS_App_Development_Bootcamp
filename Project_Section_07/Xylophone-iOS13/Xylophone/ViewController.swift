7bed0bd1b436ca86c1d5fe729de31095007d2b29
KasRoid <KasRoid@gmail.com>
2020-05-05 13:52:13 +0900

    Section 7.75 DONE

--- Project_Section_07/Xylophone-iOS13/Xylophone/ViewController.swift
+++ Project_Section_07/Xylophone-iOS13/Xylophone/ViewController.swift
@@ -14,7 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print("I got pressed")
+    }
+    
     
 
 }
