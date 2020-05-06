b86c1ddc5618815e1404ce5d0abf5104d30004d9
KasRoid <KasRoid@gmail.com>
2020-05-06 20:29:04 +0900

    Section 9.107 DONE

--- Project_Section_09/Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
+++ Project_Section_09/Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -40,17 +40,24 @@ class ViewController: UIViewController {
         let actualAnswer = quiz[questionNumber].answer
         
         if userAnswer == actualAnswer && questionNumber + 1 < quiz.count {
+            sender.backgroundColor = UIColor.green
             questionNumber += 1
-            updateUI()
         }
         else if userAnswer == actualAnswer && questionNumber + 1 == quiz.count {
             questionNumber = 0
-            updateUI()
         }
+        else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI) , userInfo: nil, repeats: false)
     }
     
-    func updateUI() {
+    @objc func updateUI() {
         questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
     }
     
 }
