6ce7a87ca230aa460c2cf86f38a4cb7737a7241d
KasRoid <KasRoid@gmail.com>
2020-05-06 10:38:13 +0900

    Section 8.87 DONE

--- Project_Section_08/EggTimer-iOS13/EggTimer/ViewController.swift
+++ Project_Section_08/EggTimer-iOS13/EggTimer/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
+    @IBAction func eggButtonPressed(sender: UIButton) {
+        let hardness = sender.currentTitle
+        
+        if hardness == "Soft" {
+            print(softTime)
+        }
+        else if hardness == "Medium" {
+            print(mediumTime)
+        }
+        else if hardness == "Hard" {
+            print(hardTime)
+        }
+    }
 
 }
