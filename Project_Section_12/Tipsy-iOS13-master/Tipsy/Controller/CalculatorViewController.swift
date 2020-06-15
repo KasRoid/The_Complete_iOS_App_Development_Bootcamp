bb40bf4f346b2635c21bc33b6ca220a0da02c140
KasRoid <KasRoid@gmail.com>
2020-06-15 11:05:29 +0900

    Finished Basic Settings

--- Project_Section_12/Tipsy-iOS13-master/Tipsy/Controller/CalculatorViewController.swift
+++ Project_Section_12/Tipsy-iOS13-master/Tipsy/Controller/CalculatorViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    
+    // MARK: - Properties
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    // MARK: - Actions
     @IBAction func tipChanged(_ sender: UIButton) {
         
     }
@@ -29,5 +32,7 @@ class CalculatorViewController: UIViewController {
     @IBAction func calculatePressed(_ sender: UIButton) {
         
     }
+    
+    
 }
 
---
+++ Project_Section_12/Tipsy-iOS13-master/Tipsy/Controller/ResultViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Doyoung Song on 6/15/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    // MARK: - Properties
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        
+    }
+}
